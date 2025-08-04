7ffbd0f93906a3230a01e7c28dcf1901a24b1e53
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-04 16:59:01 +0300

    added basic asteroid logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -8,15 +10,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
@@ -30,6 +34,8 @@ def main():
             item.draw(screen)
         
         pygame.display.flip()
+
+        # limit FPS to 60
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
