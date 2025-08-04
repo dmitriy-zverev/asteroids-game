afd4c1164fd1e3daec7de22293be80fc10c74638
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-04 15:29:58 +0300

    added basic fps code

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,6 +19,7 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # FPS
 
 
 
